1cdc32465246ce924318e57d7e2ea4f2d06e8908
Mauro456 <mauro.fi@gmail.com>
2024-09-18 11:03:47 +0300

    done

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,7 +5,12 @@
         public static void Main(string[] args)
         {
             //Koodi tähän
-           
+            string nimi = "Mauro";
+            string kaupunki = "Vantaa";
+
+            Console.WriteLine("Minun nimeni on " + nimi + " ja asun kaupungissa " + kaupunki);
+            Console.WriteLine(nimi + kaupunki);
+            Console.WriteLine(nimi + " " + kaupunki);
         }
     }
 }
